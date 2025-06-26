729b31d7975d9eabf7c6a4453ca0e95c0c723582
AbrarNitk <abrar.nitk@gmail.com>
2025-06-26 21:01:01 +0530

    added the job queue

--- src/v2.rs
+++ src/v2.rs
@@ -1,3 +1,5 @@
+use crossbeam::queue::SegQueue;
+use std::sync::Arc;
 use std::thread::JoinHandle;
 
 pub type Job = Box<dyn Fn() + Send + 'static>;
@@ -8,18 +10,26 @@ pub enum Message {
 }
 
 pub struct ThreadPool {
+    /// Worker Thread
     pub workers: Vec<Worker>,
+
+    /// Job queue
+    pub queue: Arc<SegQueue<Message>>,
 }
 
 impl ThreadPool {
     pub fn new(count: usize) -> Self {
+        let queue = Arc::new(SegQueue::new());
+
+        let q = std::sync::Arc::clone(&queue);
+
         let mut workers = vec![];
         for id in 0..count {
-            let worker = Worker::new(id + 1);
+            let worker = Worker::new(id + 1, std::sync::Arc::clone(&queue));
             workers.push(worker);
         }
 
-        Self { workers }
+        Self { workers, queue }
     }
 }
 
@@ -29,8 +39,16 @@ pub struct Worker {
 }
 
 impl Worker {
-    fn new(id: usize) -> Self {
-        let thread = std::thread::spawn(|| {});
+    fn new(id: usize, queue: Arc<SegQueue<Message>>) -> Self {
+        let thread = std::thread::spawn(move || loop {
+            match queue.pop() {
+                Some(Message::Exit) => break,
+                Some(Message::Job(job)) => {
+                    job();
+                }
+                None => {}
+            }
+        });
 
         Worker {
             id: format!("worker-{}", id),
